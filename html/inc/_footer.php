3c8ccc85cb7e53f86599f44e98dc590395f43ff9
Kevin Haire <khaire@shopcity.com>
2014-02-04 14:12:04 -0500

    initial commit

---
+++ html/inc/_footer.php
@@ -0,0 +1,6 @@
+<!-- FOOTER CONTENT -->
+
+<!-- END OF FOOTER CONTENT -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://www.shop<?php echo ($bizCity); ?>.<?php echo ($ext); ?>/webtools/script/all.js.cfm"></script>
+<script type="text/javascript" src="js/script.js"></script>
